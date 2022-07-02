5d81967b6add37033c18974976e7ff1b6b79794c
Roger Coll <rogercoll@protonmail.com>
2022-07-02 20:20:05 +0200

    feat: improve stats concurrency

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/rogercoll/ebpfutil
 
 go 1.18
 
-require golang.org/x/sys v0.0.0-20220702020025-31831981b65f // indirect
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220702020025-31831981b65f // indirect
+)
--- stats.go
+++ stats.go
@@ -1,5 +1,12 @@
 package ebpfutil
 
+import (
+	"fmt"
+	"sync"
+
+	"go.uber.org/multierr"
+)
+
 type BPFProgram struct {
 	// ID of the BPF program
 	ID uint32
@@ -9,27 +16,52 @@ type BPFProgram struct {
 	Info *BPFProgInfo
 }
 
+type result struct {
+	program BPFProgram
+	err     error
+}
+
 // GetAllStats returns an array of the attached BPF programs and its avaialble information
 func GetAllStats() ([]BPFProgram, error) {
 	progs, err := ProgramsID()
 	if err != nil {
 		return nil, err
 	}
-	bpfprogs := make([]BPFProgram, len(progs))
-	for i, progID := range progs {
-		fd, err := GetProgFileDescriptor(progID)
-		if err != nil {
-			return nil, err
-		}
-		info, err := GetInfoByFD(fd)
-		if err != nil {
-			return nil, err
-		}
-		bpfprogs[i] = BPFProgram{
-			ID:   progID,
-			FD:   fd,
-			Info: info,
-		}
+
+	results := make(chan result, len(progs))
+	wg := &sync.WaitGroup{}
+	wg.Add(len(progs))
+	for _, progID := range progs {
+		go func(id uint32) {
+			defer wg.Done()
+			program := BPFProgram{ID: id}
+			fd, err := GetProgFileDescriptor(id)
+			if err != nil {
+				results <- result{program, err}
+				return
+			}
+			program.FD = fd
+			info, err := GetInfoByFD(fd)
+			if err != nil {
+				results <- result{program, err}
+				return
+			}
+			program.Info = info
+			results <- result{program, nil}
+		}(progID)
 	}
-	return bpfprogs, nil
+	wg.Wait()
+	close(results)
+
+	var errs error
+	var bpfprogs []BPFProgram
+	for res := range results {
+		if res.err != nil {
+			errs = multierr.Append(errs, fmt.Errorf("BPF program %d, error: %w", res.program.ID, res.err))
+			continue
+		}
+		bpfprogs = append(bpfprogs, res.program)
+	}
+
+	return bpfprogs, errs
 }
